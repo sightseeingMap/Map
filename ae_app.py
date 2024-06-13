3ab4213353ede020f32de56051b1c0084d922e0b
0hzL <123910814+0hzL@users.noreply.github.com>
2024-06-13 20:09:33 +0900

    수정 1차

--- ae_app.py
+++ ae_app.py
@@ -1,11 +1,11 @@
 from flask import Flask, render_template, request,  redirect, url_for
-import requests, xmltodict, json, jsonify 
-import os
-import re   
+from flask import requests, xmltodict, json, jsonify 
+from flask import os
+from flask import re   
 from flask_sqlalchemy import SQLAlchemy
 from flask import session
+from pymongo import MongoClient
 import hashlib
-from PyKakao import Local
 
 
 
@@ -15,22 +15,24 @@ app = Flask(__name__)
 
 app.secret_key = 'SPARTA'
 
+# MongoDB 설정
+client = MongoClient('localhost', 27017)  #127.0.0.1
+db = client.mydatabase
+collection = db.users  # 사용자 정보를 저장할 컬렉션
 
-
-@app.route('/login/')
+@app.route('/')
 def index():
     # 로그인 페이지 렌더링
     return render_template('login.html')
     
-@app.route('/join/')
+@app.route('/join2.html')
 def join():
     # 회원가입 페이지 렌더링
     return render_template('join2.html')
 
-@app.route('/')
+@app.route('/index.html')
 def map():
     # 메인 페이지 렌더링
-    Local.
     return render_template('index.html')
 
 @app.route('/register', methods=['GET', 'POST'])
@@ -48,32 +50,16 @@ def register():
         # 비밀번호를 해시화하여 보안 강화
         hashed_password = hashlib.sha256(password.encode('utf-8')).hexdigest()
 
-
+        # 사용자 정보를 MongoDB에 삽입
+        user_data = {'username': username, 'password': hashed_password, 'email': email}
+        collection.insert_one(user_data)   
 
         # 회원가입이 성공했다면 홈페이지로 리다이렉트합니다.
-        return redirect('/login/')
+        return redirect('/')
     
     # GET 요청일 경우 회원가입 페이지 렌더링
     return render_template('join2.html')
 
-@app.route('/login.html', methods=['POST'])
-def login():
-    if request.method == 'POST':
-        # 폼 데이터 추출
-        username = request.form['username']
-        password = request.form['password']
-
-        
-        # 로그인 실패 시 메시지를 출력하거나 로그인 페이지로 리다이렉트
-        return "로그인 실패. 사용자 이름 또는 비밀번호를 확인하세요.", 401
-    # 로그인 페이지 렌더링
-    return render_template('login.html')
-
-@app.route('/logout')
-def logout():
-    # 세션에서 사용자 정보 제거 (로그아웃)
-    session.pop('username', None)
-    return redirect('/')
 
 if __name__ == '__main__':
     app.run(debug=True)
